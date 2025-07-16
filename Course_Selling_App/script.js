9eac9a9c62be957750acd31c1ea782588845ad4d
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-07-16 17:32:09 +0530

    Update script.js

--- Course_Selling_App/script.js
+++ Course_Selling_App/script.js
@@ -1,1 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json())
 
+const mongoose = require("mongoose");
+
+
+const { userRouter } = require("./router/user");
+const { courseRouter } = require("./router/course");
+const { adminRouter } = require("./router/admin");
+
+
+
+app.use("/api/v1/user" , userRouter);
+app.use("/api/v1/admin" , adminRouter);
+app.use("/api/v1/course" , courseRouter);
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(3000);
+}
+
+main();
