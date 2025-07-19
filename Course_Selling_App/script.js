1ca35e299fdd8bb61a1d17b66221929adf2ad550
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-07-19 19:50:04 +0530

    Update script.js

--- Course_Selling_App/script.js
+++ Course_Selling_App/script.js
@@ -6,15 +6,23 @@ app.use(express.json())
 const mongoose = require("mongoose");
 
 
-const { userRouter } = require("./router/user");
-const { courseRouter } = require("./router/course");
-const { adminRouter } = require("./router/admin");
+const { userRouter } = require("./router/user_router.js");
+const { courseRouter } = require("./router/course_router.js");
+const { adminRouter } = require("./router/admin_router.js");
 
 
 
 app.use("/api/v1/user" , userRouter);
 app.use("/api/v1/admin" , adminRouter);
 app.use("/api/v1/course" , courseRouter);
+
+
+    app.use((err, req, res, next) => {
+        console.error(err); // Log the error for debugging
+        const statusCode = err.statusCode || 500;
+        const message = err.message || "An internal server error occurred.";
+        res.status(statusCode).json({ message });
+    });
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL);
